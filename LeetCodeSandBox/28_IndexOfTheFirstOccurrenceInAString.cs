b322776a2d818c1fcb12e1553f381cf81a26bfb5
LordOfTheDeadlines <vedroid28@mail.ru>
2024-10-29 15:02:50 +0300

    the 28th task was added

---
+++ LeetCodeSandBox/28_IndexOfTheFirstOccurrenceInAString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCodeSandBox;
+
+public class IndexOfTheFirstOccurrenceInAStringTask
+{
+    public static int StrStr(string haystack, string needle)
+    {
+        var substringPattern = new Regex($"({needle})");
+
+        if (!substringPattern.IsMatch(haystack)) return -1;
+
+        var modifiedInput = substringPattern.Replace(haystack, ".");
+
+        return modifiedInput.IndexOf('.');
+    }
+}
---
+++ LeetCodeSandBoxTests/28_IndexOfTheFirstOccurrenceInAString.cs
@@ -0,0 +1,14 @@
+﻿using LeetCodeSandBox;
+
+namespace LeetCodeSandBoxTests;
+
+public class IndexOfTheFirstOccurrenceInAStringTest
+{
+    [TestCase("sadbutsad", "sad", 0)]
+    [TestCase("leetcode", "leeto", -1)]
+    public void AssertIndexes(string haystack, string needle, int result)
+    {
+        var index = IndexOfTheFirstOccurrenceInAStringTask.StrStr(haystack, needle);
+        Assert.That(index, Is.EqualTo(result));
+    }
+}
