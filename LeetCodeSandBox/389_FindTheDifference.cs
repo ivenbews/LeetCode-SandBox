11a3da23e279af90fb1be72fa07025792ac48024
LordOfTheDeadlines <vedroid28@mail.ru>
2024-10-29 14:36:24 +0300

    FindTheDifference task solution was added

---
+++ LeetCodeSandBox/389_FindTheDifference.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+
+namespace LeetCodeSandBox;
+
+public class FindTheDifferenceTask
+{
+    public static char FindTheDifference(string s, string t)
+    {
+        if (string.IsNullOrEmpty(s)) return t[^1];
+
+        var sGroupedChars = s.GroupBy((x) => x);
+        var tGroupedChars = t.GroupBy((x) => x);
+
+        foreach (var tElement in tGroupedChars)
+        {
+            var tKey = tElement.Key;
+            var sElement = sGroupedChars.FirstOrDefault(x => x.Key.Equals(tKey));
+
+            if (sElement is null || sElement.Count() != tElement.Count())
+            {
+                return tKey;
+            }
+        }
+
+        return t[^1];
+    }
+}
