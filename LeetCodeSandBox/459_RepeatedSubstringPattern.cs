34b93b12b47a7793ed151250e6815b9ecaf2a078
LordOfTheDeadlines <vedroid28@mail.ru>
2024-10-29 17:58:19 +0300

    459th task solution was added

---
+++ LeetCodeSandBox/459_RepeatedSubstringPattern.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSandBox;
+
+public class RepeatedSubstringPatternTask
+{
+    public static bool RepeatedSubstringPattern(string s)
+    {
+        int maxSubstringLength = s.Length / 2;
+
+        for (int substringLength = maxSubstringLength; substringLength > 0; substringLength--)
+        {
+            if (s.Length % substringLength != 0) continue;
+
+            var hasRepeatedSubstrings = HasRepeatedSubstrings(s, null, 0, substringLength);
+            if (hasRepeatedSubstrings) return true;
+        }
+
+        return false;
+    }
+
+    private static bool HasRepeatedSubstrings(string s, string substring, int startIndex, int substringLength)
+    {
+        if (startIndex >= s.Length) return true;
+
+        var substringForCompare = s.Substring(startIndex, substringLength);
+
+        if (substring is not null && !substring.Equals(substringForCompare))
+        {
+            return false;
+        }
+
+        return HasRepeatedSubstrings(s, substringForCompare, startIndex + substringLength, substringLength);
+    }
+}
---
+++ LeetCodeSandBoxTests/459_RepeatedSubstringPattern.cs
@@ -0,0 +1,19 @@
+﻿using LeetCodeSandBox;
+
+namespace LeetCodeSandBoxTests;
+
+public class RepeatedSubstringPatternTest
+{
+    [TestCase("abab", true)]
+    [TestCase("aba", false)]
+    [TestCase("abcabcabcabc", true)]
+    [TestCase("abac", false)]
+    [TestCase("bb", true)]
+    [TestCase("ababab", true)]
+    [TestCase("ababba", false)]
+    public void AssertIndexes(string s, bool result)
+    {
+        var hasRepeated = RepeatedSubstringPatternTask.RepeatedSubstringPattern(s);
+        Assert.That(hasRepeated, Is.EqualTo(result));
+    }
+}
