373bd314cf5b63e2bd5c85417f13351a79dc437f
LordOfTheDeadlines <vedroid28@mail.ru>
2024-10-29 13:13:59 +0300

    solution with projects were added

---
+++ LeetCodeSandBox/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCodeSandBox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
