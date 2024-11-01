4f546dc845fc9aa4d9e47b54dba4cfa885039e44
LordOfTheDeadlines <vedroid28@mail.ru>
2024-11-01 16:04:43 +0300

    66th task was resolved

---
+++ LeetCodeSandBox/66_PlusOne.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LeetCodeSandBox;
+
+public class PlusOneTask
+{
+    public static int[] PlusOne(int[] digits)
+    {
+        if (digits[^1] != 9)
+        {
+            digits[^1]++;
+            return digits;
+        }
+
+        if (digits.All(x => x == 9))
+        {
+            var increasedArray = new int[digits.Length + 1];
+            increasedArray[0] = 1;
+            return increasedArray;
+        }
+
+        var result = new int[digits.Length];
+
+        return GetIncrement(digits, result, digits.Length - 1, 0);
+    }
+
+    private static int[] GetIncrement(int[] inputArray, int[] result, int index, int increment)
+    {
+        if (index < 0) return result;
+
+        var currentValue = inputArray[index] + increment;
+        var isLastDigit = index == result.Length - 1;
+
+        if (currentValue < 9)
+        {
+            result[index] = isLastDigit ? ++currentValue : currentValue;
+            return GetIncrement(inputArray, result, index - 1, 0);
+        }
+
+        if (currentValue == 9 && !isLastDigit)
+        {
+            result[index] = currentValue;
+            return GetIncrement(inputArray, result, index - 1, 0);
+        }
+
+        result[index] = 0;
+        return GetIncrement(inputArray, result, index - 1, 1);
+    }
+}
---
+++ LeetCodeSandBoxTests/66_PlusOne.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeSandBox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSandBoxTests;
+
+public class PlusOneTest
+{
+    [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 2, 4 })]
+    [TestCase(new int[] { 4, 3, 2, 1 }, new int[] { 4, 3, 2, 2 })]
+    [TestCase(new int[] { 9 }, new int[] { 1, 0 })]
+    [TestCase(new int[] { 3, 9, 9, 9 }, new int[] { 4, 0, 0, 0 })]
+    [TestCase(new int[] { 8, 9, 9, 9 }, new int[] { 9, 0, 0, 0 })]
+    [TestCase(new int[] { 2, 4, 9 }, new int[] { 2, 5, 0 })]
+    [TestCase(new int[] { 9, 8, 9 }, new int[] { 9, 9, 0 })]
+    [TestCase(
+        new int[] { 7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 6 }, 
+        new int[] { 7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 7 }
+    )]
+    public void AssertArrays(int[] nums, int[] result)
+    {
+        var res = PlusOneTask.PlusOne(nums);
+        Assert.That(res, Is.EquivalentTo(result));
+    }
+}
